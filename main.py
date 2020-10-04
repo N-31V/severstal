20509a90df54726f1ce300397de8be50d2a91265
N-31V <litvintseva_ann@mail.ru>
2020-10-04 14:53:48 +0300

    add check file for jetson

---
+++ main.py
@@ -0,0 +1,12 @@
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+import torch
+import torchvision
+import pickle
+
+if __name__ == '__main__':
+    print('cuda is available:', torch.cuda.is_available())
+    device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+    print(device, (torch.cuda.get_device_name(0) if torch.cuda.is_available() else ''))
