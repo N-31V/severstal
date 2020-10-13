dbe81be47d487850e81a589f94274faf72db1405
N-31V <litvintseva.ann@gmail.com>
2020-10-13 20:20:11 +0300

    updated args

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 
+import argparse
 import segmentation_models_pytorch as smp
 from training import Trainer
 from other_func import plot_score
@@ -18,7 +19,7 @@ if __name__ == '__main__':
     parser = createParser()
     args = parser.parse_args()
     model = smp.Unet("resnet18", encoder_weights="imagenet", classes=4, activation=None)
-    model_trainer = Trainer(model, "./input/", "./input/train.csv", num_epochs=args.epochs, num_workers=args.num_workers, batcg_size=args.batch_size)
+    model_trainer = Trainer(model, "./input/", "./input/train.csv", num_epochs=args.epochs, num_workers=args.num_workers, batch_size=args.batch_size)
     model_trainer.start()
 
     plot_score(model_trainer.losses, "BCE loss")
