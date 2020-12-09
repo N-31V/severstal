cd83dbe00fbba0c35d6ea262fab0cd04c66a140b
N-31V <litvintseva.ann@gmail.com>
2020-12-10 02:01:37 +0300

    added model loading for continuation training

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 
 import argparse
 import segmentation_models_pytorch as smp
-from work_with_model import ModelToolkit
+from work_with_model import ModelToolkit, load_model
 
 
 def create_parser():
@@ -10,12 +10,16 @@ def create_parser():
     argparser.add_argument('-e', '--epochs', type=int, default=20, help='int')
     argparser.add_argument('-n', '--num_workers', type=int, default=4, help='int')
     argparser.add_argument('-b', '--batch_size', type=int, default=4, help='int')
+    argparser.add_argument('-m', '--model', type=str, default=None, help='model path')
     return argparser
 
 
 if __name__ == '__main__':
     parser = create_parser()
     args = parser.parse_args()
-    model = smp.Unet('resnet18', encoder_weights='imagenet', classes=4, activation=None)
-    model = ModelToolkit(model, 'Unet', num_workers=args.num_workers, batch_size=args.batch_size)
+    if args.model is None:
+        model = smp.Unet('resnet18', encoder_weights='imagenet', classes=4, activation=None)
+        model = ModelToolkit(model, 'Unet', num_workers=args.num_workers, batch_size=args.batch_size)
+    else:
+        model = load_model(args.model)
     model.train(args.epochs)
